7cb6e05361dac3f75a1cf8d3db88cf7c670cacc6
JanhaveeSingh <167533024+JanhaveeSingh@users.noreply.github.com>
2025-03-02 19:11:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Route for checking if the API is running
 @app.route('/', methods=['GET'])
 def home():
     return "Flask API is running!", 200
+
+# Handle OPTIONS requests for CORS preflight
+@app.route('/', methods=['OPTIONS'])
+def options():
+    response = jsonify({})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    return response, 200
 
 # POST route for processing input
 @app.route('/', methods=['POST'])
