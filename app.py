e4c2222fdb0f53e12293c39c198d0a022a47ae9f
JanhaveeSingh <167533024+JanhaveeSingh@users.noreply.github.com>
2025-03-03 05:29:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -93,9 +93,9 @@ vector_index = Neo4jVector.from_existing_graph(
     node_label="Document",
     text_node_properties=["text"],
     embedding_node_property="embedding",
-    url=os.getenv("NEO4J_URI"),
-    username=os.getenv("NEO4J_USERNAME"),
-    password=os.getenv("NEO4J_PASSWORD")
+    url=NEO4J_URI,
+    username=NEO4J_USERNAME,
+    password=NEO4J_PASSWORD
 )
 
 # Define entity extraction class
